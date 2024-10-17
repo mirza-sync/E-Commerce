76c2ab1fdd31593caf58bce715ebc12131559a8b
mirza-sync <mirzasyahmi123@gmail.com>
2024-10-17 23:35:33 +0800

    Fix product deactivated after update

--- src/app/admin/_actions/products.ts
+++ src/app/admin/_actions/products.ts
@@ -78,7 +78,6 @@ export async function updateProduct(id: string, prevState: unknown, formData: Fo
   await db.product.update({
     where: { id },
     data: {
-      isAvailableForPurchase: false,
       name: data.name,
       description: data.description,
       priceInCents: data.priceInCents,
