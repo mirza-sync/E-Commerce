d70f3c9d9f2c61f85da3e05911fec868b6f13c7a
mirza-sync <mirzasyahmi123@gmail.com>
2024-11-11 23:32:38 +0800

    Init stripe

--- src/app/(customerFacing)/products/[id]/purchase/page.tsx
+++ src/app/(customerFacing)/products/[id]/purchase/page.tsx
@@ -1,3 +1,21 @@
-export default function PurchasePage() {
+import db from '@/db';
+import { notFound } from 'next/navigation';
+import Stripe from 'stripe';
+
+export default async function PurchasePage({ params: { id } }: { params: { id: string } }) {
+  const product = await db.product.findUnique({ where: { id } });
+  if (product == null) return notFound();
+
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: product.priceInCents,
+    currency: 'USD',
+    metadata: { product: product.id },
+  });
+
+  if (paymentIntent.client_secret == null) {
+    throw Error('Stripe failed to create payment intent');
+  }
+
   return <div>Purchase page</div>;
 }
